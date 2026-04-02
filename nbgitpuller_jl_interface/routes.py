5c97f2a0f1ebe58d4f83d4ad4514e58a6df9f7d2
Sammy Gallagher <89541643+segallagher@users.noreply.github.com>
2026-04-01 16:56:25 -0800

    Add test commit comment in routes.py
    Added a test commit comment in the routes.py file.

--- nbgitpuller_jl_interface/routes.py
+++ nbgitpuller_jl_interface/routes.py
@@ -6,6 +6,8 @@ from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
 import tornado
 
+# Test commit
+
 class HelloRouteHandler(APIHandler):
     # The following decorator should be present on all verb methods (head, get, post,
     # patch, put, delete, options) to ensure only authorized user can request the
